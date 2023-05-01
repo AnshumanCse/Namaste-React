f442349188a42693189b3e4222fa2daad7e35e1a
Anshuman Tiwari <112653339+AnshumanCse@users.noreply.github.com>
2023-04-30 23:37:18 -0600

    w. Arrow Fun
    try to write Arrow function.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import FirstComponet from "./FirstComponet";
+// import FirstComponet from "./FirstComponet";
 import SecondComonet from "./SecondComonet";
 
-function App(){
-    return(
+// function App(){
+//     return(
+//         <>
+//         <SecondComonet/>
+//         </>
+//     )
+// }
+const App = () => {
+    return (
         <>
-        <SecondComonet/>
+            <SecondComponent />
         </>
     )
 }
+
+
 
 export default App;
